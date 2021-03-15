82d824a9c354c2c653c3048f18f1722b5e89d3d9
GAJJALA DEEPAK <iib2019024@iiita.ac.in>
2021-03-15 17:47:38 +0530

    implemented backend authentication

---
+++ server/routes/auth.js
@@ -0,0 +1,87 @@
+const router = require('express').Router();
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const verifyToken = require('../middlewares/verify-token');
+
+/* Signup Route */
+router.post('/auth/signup',async(req,res) => {
+    if(!req.body.email || !req.body.password) {
+        res.json({success: false, message: "Please Enter email or password"});
+    } else {
+        try {
+            let newUser = new User();
+            newUser.name = req.body.name;
+            newUser.email = req.body.email;
+            newUser.password = req.body.password;
+            await newUser.save(); 
+            let token = jwt.sign(newUser.toJSON(), process.env.SECRET, {
+                expiresIn: 604800 // 1 week
+            });
+
+            res.json({
+                success: true,
+                token: token,
+                message: "Successfully created a new user",
+            });
+        } catch(err){
+            res.status(500).json({
+                success: false,
+                message: err.message
+            });
+        }
+    }
+});
+
+/* profile route */
+router.get("/auth/user", verifyToken, async(req, res) => {
+    try {
+        let foundUser = await User.findOne({ _id: req.decoded._id});
+        if(foundUser) {
+            res.json({
+                success: true,
+                message: foundUser
+            });
+        }
+    } catch(err){
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });   
+    }
+});
+
+/* Login Route */
+router.post("/auth/login", async(req,res) => {
+    try{
+        let foundUser = await User.findOne({ email: req.body.email});
+        if(!foundUser){
+            res.status(403).json({
+                success: gfalse,
+                message: "Authentication failed, USer not found"
+            });
+        } else {
+            if(foundUser.comparePassword(req.body.password)) {
+                let token = jwt.sign(foundUser.toJSON(), process.env.SECRET, {
+                    expiresIn: 604800 // 1 week
+                });
+
+                res.json({ 
+                    success: true, 
+                    token: token 
+                });
+            } else {
+                res.status(403).json({
+                    success: false,
+                    message: err.message
+                });
+            }
+        }
+    } catch(err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-//const dotenv = require("dotenv");
+const dotenv = require("dotenv");
 const cors = require("cors");
 
 const User = require("./models/user");
 
-//dotenv.config();
+dotenv.config();
 
 const app = express();
 
@@ -33,10 +33,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const productRoutes = require("./routes/product");
 const categoryRoutes = require("./routes/category");
 const ownerRoutes = require("./routes/owner");
+const userRoutes = require("./routes/auth");
 
 app.use("/api", productRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", ownerRoutes);
+app.use("/api", userRoutes);
 
 app.listen(3000, (err) => {
   if (err) {
