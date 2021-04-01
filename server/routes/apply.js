ea6c34d1ca858c0fccb5b0ad4b65b863c11237fc
Krishna Kaipa <kaipakrishna380@gmail.com>
2021-04-02 00:02:10 +0400

    Added check for duplicate internships. Works for checking duplicate students as well.

--- server/routes/apply.js
+++ server/routes/apply.js
@@ -6,11 +6,9 @@ const jwt = require('jsonwebtoken');
 //has to pass in internship id in req.body.internship_id and cookie in req.body.cookie
 router.put("/apply", async(req,res) => {
     try{
-        // console.log("hello");
+        // console.log(req.body.internship_id);
         let interncard = await Interncard.findOne({ _id: req.body.internship_id });
-        // console.log(interncard);
         let cookie = req.body.cookie;
-        // console.log(cookie);
         let decoded_user;
         jwt.verify(cookie, process.env.SECRET, (err,decoded) => {
             if(err){
@@ -26,34 +24,52 @@ router.put("/apply", async(req,res) => {
             "adress"
         );
         let user_internships = found_user.internships;
+        // console.log(interncard);
+        let flag = 0;
         //add check for duplicates
-        user_internships.push(interncard);
-        let user = await  User.findOneAndUpdate(
-            { _id: decoded_user._id },
+        for(let i=0;i<user_internships.length;i++){
+            if(user_internships[i]._id==req.body.internship_id)
             {
-                $set: {
-                    internships: user_internships,
+                flag = 1;
+            }
+        }
+        if(flag===0)
+        {
+            user_internships.push(interncard);
+            let user = await  User.findOneAndUpdate(
+                { _id: decoded_user._id },
+                {
+                    $set: {
+                        internships: user_internships,
+                    },
                 },
-            },
-            { upsert: true }
-        );
-        let internship_studs = interncard.appliedStudents;
-        internship_studs.push(decoded_user.email);
-        let internship = await Interncard.findOneAndUpdate(
-            { _id: req.body.internship_id },
-            {
-                $set: {
-                    appliedStudents: internship_studs,
+                { upsert: true }
+            );
+            let internship_studs = interncard.appliedStudents;
+            internship_studs.push(decoded_user.email);
+            let internship = await Interncard.findOneAndUpdate(
+                { _id: req.body.internship_id },
+                {
+                    $set: {
+                        appliedStudents: internship_studs,
+                    },
                 },
-            },
-            { upsert: true }
-        );
-        res.json({
-            success: true,
-            message: "Internship added.",
-            updatedUser: user,
-            updatedInternship: internship,
-        });
+                { upsert: true }
+            );
+            res.json({
+                success: true,
+                message: "Internship added.",
+                updatedUser: user,
+                updatedInternship: internship,
+            });
+        }
+        else
+        {
+            res.json({
+                success: true,
+                message: "Internship exists",
+            });
+        }
     } catch(error) {
         res.status(500).json({
             success: false,
